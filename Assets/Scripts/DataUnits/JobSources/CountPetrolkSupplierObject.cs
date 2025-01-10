533198af3d28d9232b41c783700e4318f2223c90
orloKun <oguerrerofarias@gmail.com>
2025-01-10 13:17:37 -0300

    Fixed issue with wait time for call answer

---
+++ Assets/Scripts/DataUnits/JobSources/CountPetrolkSupplierObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using DialogueSystem.Interfaces;
+using GameDirection;
+using GamePlayManagement.BitDescriptions.Suppliers;
+using Random = UnityEngine.Random;
+
+namespace DataUnits.JobSources
+{
+    [Serializable]
+    public class CountPetrolkSupplierObject : JobSupplierObject
+    {
+        private CountPetrolkDialogueStates _mDialogueState;
+        public CountPetrolkDialogueStates DialogueState => _mDialogueState;
+        
+
+
+        public override void LocalInitialize(JobSupplierBitId id)
+        {
+            base.LocalInitialize(id);
+            _mDialogueState = CountPetrolkDialogueStates.WaitingForHire;
+        }
+        
+        protected override async void BuildResponseAndAnswer()
+        {
+            base.BuildResponseAndAnswer();
+            if (_dialogueModule.ImportantDialogues.Any(x => x.Value.TimesActivatedCount == 0 && x.Value.GetDialogueAssignedStatus == (int)DialogueState))
+            {
+                var importantDialogue = _dialogueModule.ImportantDialogues.FirstOrDefault(x => x.Value.TimesActivatedCount == 0 && x.Value.GetDialogueAssignedStatus == (int)DialogueState).Value;
+                AnswerPhoneWithDialogueReady(importantDialogue);
+                return;
+            }
+            var insistenceDialoguesCount = _dialogueModule.InsistenceDialogues.Count;
+            Random.InitState(DateTime.Now.Millisecond);
+            var randomInsistenceIndex = Random.Range(1, insistenceDialoguesCount+1);
+            var resultDialogue = _dialogueModule.InsistenceDialogues[randomInsistenceIndex];
+            AnswerPhoneWithDialogueReady(resultDialogue);
+        }
+        
+
+        private void AnswerPhoneWithDialogueReady(IDialogueObject answer)
+        {
+            PhoneCallOperator.Instance.PlayAnswerSound();
+            GameDirector.Instance.GetDialogueOperator.StartNewDialogue(answer);
+        }
+
+    }
+}
