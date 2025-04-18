bad47511293d36f268e717d641b986db8aa1a308
orloKun <oguerrerofarias@gmail.com>
2025-04-18 11:50:40 -0400

    voice text

--- Assets/Scripts/GameDirection/TimeOfDayManagement/ICalendarModule.cs
+++ Assets/Scripts/GameDirection/TimeOfDayManagement/ICalendarModule.cs
@@ -5,7 +5,6 @@ namespace GameDirection.TimeOfDayManagement
     public interface ICalendarModule : IProfileModule
     {
         public void SetCurrentWorkDayObject(DayBitId newDay);
-
         public IWorkDayObject GetCurrentWorkDayObject();
         public IWorkDayObject GetNextWorkDayObject();
         public PartOfDay GetCurrentPartOfDay { get; }
